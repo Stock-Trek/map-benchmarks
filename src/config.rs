5aeb67867e431996fecaf850c4baf3a8514b9e69
Nathan <an.email.com>
2026-07-28 14:49:42 +0100

    add 100K entry count

--- src/config.rs
+++ src/config.rs
@@ -2,4 +2,4 @@ use std::time::Duration;
 
 pub const WARM_UP_TIME: Duration = Duration::from_secs(1);
 pub const MEASUREMENT_TIME: Duration = Duration::from_secs(2);
-pub const ENTRY_COUNT: &[usize] = &[10_000];
+pub const ENTRY_COUNT: &[usize] = &[10_000, 100_000];
