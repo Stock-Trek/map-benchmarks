a474471b846eb7da56729bc1329a1d25f7c3e7a8
Nathan <an.email.com>
2026-07-31 13:34:43 +0100

    add 6 thread benchmark

--- src/config.rs
+++ src/config.rs
@@ -6,7 +6,7 @@ pub const MEASUREMENT_TIME: Duration = Duration::from_secs(2);
 pub const BASELINE_ENTRY_COUNT: &[usize] = &[10_000, 100_000, 1_000_000];
 
 pub const CONCURRENCY_OPS_PER_THREAD: usize = 10_000;
-pub const CONCURRENCY_THREAD_COUNTS: &[usize] = &[1, 2, 4];
+pub const CONCURRENCY_THREAD_COUNTS: &[usize] = &[1, 2, 4, 6];
 
 pub const KEY_SENSITIVITY_ENTRY_COUNT: usize = 100_000;
 
