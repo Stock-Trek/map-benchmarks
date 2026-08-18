7d38414d2651b3ca143b70f339a7bff89eb4f484
Nathan J Adams <nathanjadams1@gmail.com>
2026-08-18 09:15:12 +0100

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -3,7 +3,7 @@ use std::time::Duration;
 pub const WARM_UP_TIME: Duration = Duration::from_secs(1);
 pub const MEASUREMENT_TIME: Duration = Duration::from_secs(2);
 
-pub const OUT_OF_THE_BOX_ENTRY_COUNT: &[usize] = &[10_000, 100_000, 1_000_000];
+pub const OUT_OF_THE_BOX_ENTRY_COUNT: &[usize] = &[100, 10_000, 1_000_000];
 
 pub const SAME_HASHER_MAP_COUNT: usize = 1_000_000;
 
