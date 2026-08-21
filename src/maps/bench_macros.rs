c340ab2b525acde3e74d55f7297a5b92027f09a9
Nathan <an.email.com>
2026-08-21 12:25:02 +0100

    polish param order, add hasher macro

--- src/maps/bench_macros.rs
+++ src/maps/bench_macros.rs
@@ -3,7 +3,7 @@ macro_rules! expand_bench {
     ($bench_fn:ident, $group:expr, $($bench_type:ty),* $(,)?) => {
         $(
             $bench_fn::<$bench_type>(
-                $group, <$bench_type as crate::benchmap::BenchMapName>::NAME
+                <$bench_type as crate::benchmap::BenchMapName>::NAME, $group
             );
         )*
     };
@@ -14,7 +14,18 @@ macro_rules! expand_bench_with_map_data {
     ($bench_fn:ident, $group:expr, $map_data:expr, $($bench_type:ty),* $(,)?) => {
         $(
             $bench_fn::<$bench_type>(
-                $group, $map_data, <$bench_type as crate::benchmap::BenchMapName>::NAME
+                <$bench_type as crate::benchmap::BenchMapName>::NAME, $group, $map_data
+            );
+        )*
+    };
+}
+
+#[macro_export]
+macro_rules! expand_bench_with_map_data_and_hasher {
+    ($bench_fn:ident, $group:expr, $map_data:expr, $hasher:expr, $($bench_type:ty),* $(,)?) => {
+        $(
+            $bench_fn::<$bench_type>(
+                <$bench_type as crate::benchmap::BenchMapName>::NAME, $group, $map_data, $hasher.clone()
             );
         )*
     };
