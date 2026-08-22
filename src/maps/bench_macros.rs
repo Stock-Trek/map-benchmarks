53775624839be5e0af30ff222aef00c04a2348c1
Nathan <an.email.com>
2026-08-22 10:40:01 +0100

    bugfix

--- src/maps/bench_macros.rs
+++ src/maps/bench_macros.rs
@@ -23,8 +23,8 @@ macro_rules! expand_bench_with_map_data {
 #[macro_export]
 macro_rules! expand_bench_with_map_data_and_common_hasher {
     ($bench_fn:ident, $group:expr, $map_data:expr, $($bench_type:ty),* $(,)?) => {
+        let hasher = $crate::common_hasher::CommonHasher::new();
         $(
-            let hasher = $crate::common_hasher::CommonHasher::new();
             $bench_fn::<$bench_type>(
                 <$bench_type as $crate::maps::BenchMapName>::NAME, $group, $map_data, hasher.clone()
             );
